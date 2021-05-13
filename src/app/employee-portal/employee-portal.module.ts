fd923f8fc0ca5a29d2251bbead656951393e1209
theWellHopeErr <ssuryarajan@gmail.com>
2021-05-13 13:59:11 +0530

    Add Dashboard for Employee Portal

--- src/app/employee-portal/employee-portal.module.ts
+++ src/app/employee-portal/employee-portal.module.ts
@@ -1,13 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+
 import { EmployeePortalLandingComponent } from './landing/landing.component';
 import { SharedModule } from '../shared/shared.module';
 import { EmployeePortalRoutingModule } from './employee-portal-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [EmployeePortalLandingComponent, DashboardComponent],
-  imports: [CommonModule, SharedModule, EmployeePortalRoutingModule],
+  declarations: [EmployeePortalLandingComponent, DashboardComponent, HomeComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    EmployeePortalRoutingModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+  ],
 })
 export class EmployeePortalModule {}
