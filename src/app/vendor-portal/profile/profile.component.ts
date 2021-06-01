076db58fd6449f143a29da6345329940e3bb0289
theWellHopeErr <ssuryarajan@gmail.com>
2021-06-01 15:55:33 +0530

    Add View Profile and Edit Profile in Vendor Portal

--- src/app/vendor-portal/profile/profile.component.ts
+++ src/app/vendor-portal/profile/profile.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { SnackService } from 'src/app/shared/snack.service';
+
+import { ProfileService } from '../services/profile.service';
+import { Profile } from './profile';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+  profileForm: FormGroup;
+  isEditEnabled = false;
+  saveLoading = false;
+  formLoading = true;
+  form: Profile = {
+    vid: '',
+    name1: '',
+    name2: '',
+    city: '',
+    region: '',
+    district: '',
+    street: '',
+    pcode: '',
+    country: '',
+    lang: '',
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private profileService: ProfileService,
+    private snackService: SnackService,
+    private titleService: Title
+  ) {
+    this.titleService.setTitle('Profile | Vendor Portal');
+    this.profileService.get().subscribe((res: Profile) => {
+      this.form = res;
+      this.profileForm = this.formBuilder.group(this.form);
+      this.formLoading = false;
+    });
   }
 
+  ngOnInit(): void {
+    this.profileForm = this.formBuilder.group(this.form);
+  }
+
+  edit(): void {
+    this.isEditEnabled = true;
+  }
+
+  save(): void {
+    this.saveLoading = true;
+
+    this.form = this.profileForm.value;
+    this.profileService.edit(this.profileForm.value).subscribe((res) => {
+      this.isEditEnabled = false;
+      this.saveLoading = false;
+      this.snackService.openSnackBar(`Profile Updated!!`);
+    });
+  }
+
+  cancel(): void {
+    this.profileForm.setValue(this.form);
+    this.isEditEnabled = false;
+  }
 }
---
+++ src/app/vendor-portal/profile/profile.ts
@@ -0,0 +1,12 @@
+export interface Profile {
+  vid: string;
+  name1: string;
+  name2: string;
+  street: string;
+  city: string;
+  region: string;
+  district: string;
+  pcode: string;
+  country: string;
+  lang: string;
+}
