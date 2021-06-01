d17dd4135bf320aa0c0dc62a36ffbf1c1403ebdb
theWellHopeErr <ssuryarajan@gmail.com>
2021-06-01 15:56:40 +0530

    Add MM components in Vendor Portal

--- src/app/vendor-portal/vendor-portal-routing.module.ts
+++ src/app/vendor-portal/vendor-portal-routing.module.ts
@@ -5,6 +5,13 @@ import { HomeComponent } from './home/home.component';
 import { AuthGuard } from '../shared/auth.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
+import { FincancialSheetComponent } from './fincancial-sheet/fincancial-sheet.component';
+import { PaymentsAgingComponent } from './payments-aging/payments-aging.component';
+import { CdMemoComponent } from './cd-memo/cd-memo.component';
+import { ReceiptComponent } from './receipt/receipt.component';
+import { PurchaseOrderComponent } from './purchase-order/purchase-order.component';
+import { QuotationComponent } from './quotation/quotation.component';
+import { InvoiceComponent } from './invoice/invoice.component';
 
 const routes: Routes = [
   {
@@ -20,10 +27,35 @@ const routes: Routes = [
         path: 'dashboard',
         component: DashboardComponent,
       },
-      // {
-      //   path: 'financial-sheet',
-      //   component: FinancialSheetComponent,
-      // },
+      {
+        path: 'quotation',
+        component: QuotationComponent,
+      },
+      {
+        path: 'purchase-order',
+        component:PurchaseOrderComponent,
+      },
+      {
+        path: 'receipt',
+        component: ReceiptComponent,
+      },
+
+      {
+        path: 'fincancial-sheet',
+        component: FincancialSheetComponent,
+      },
+      {
+        path: 'invoice',
+        component: InvoiceComponent,
+      },
+      {
+        path: 'payments-aging',
+        component: PaymentsAgingComponent,
+      },
+      {
+        path: 'cd-memo',
+        component: CdMemoComponent,
+      },
     ],
   },
 ];
--- src/app/vendor-portal/vendor-portal.module.ts
+++ src/app/vendor-portal/vendor-portal.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { VendorPortalLandingComponent } from './landing/landing.component';
 import { SharedModule } from '../shared/shared.module';
@@ -18,6 +23,13 @@ import { VendorPortalRoutingModule } from './vendor-portal-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { FincancialSheetComponent } from './fincancial-sheet/fincancial-sheet.component';
+import { PaymentsAgingComponent } from './payments-aging/payments-aging.component';
+import { CdMemoComponent } from './cd-memo/cd-memo.component';
+import { ReceiptComponent } from './receipt/receipt.component';
+import { PurchaseOrderComponent } from './purchase-order/purchase-order.component';
+import { QuotationComponent } from './quotation/quotation.component';
+import { InvoiceComponent } from './invoice/invoice.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +37,13 @@ import { ProfileComponent } from './profile/profile.component';
     DashboardComponent,
     HomeComponent,
     ProfileComponent,
+    FincancialSheetComponent,
+    PaymentsAgingComponent,
+    CdMemoComponent,
+    ReceiptComponent,
+    PurchaseOrderComponent,
+    QuotationComponent,
+    InvoiceComponent,
   ],
   imports: [
     CommonModule,
@@ -40,6 +59,11 @@ import { ProfileComponent } from './profile/profile.component';
     MatButtonModule,
     MatCardModule,
     MatMenuModule,
+    LayoutModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTabsModule,
   ],
 })
 export class VendorPortalModule {}
