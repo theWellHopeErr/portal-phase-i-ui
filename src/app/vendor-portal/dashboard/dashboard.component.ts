9ecb08ba72e4abaf9b0dc67f31903a9188d59c3f
theWellHopeErr <ssuryarajan@gmail.com>
2021-06-01 15:54:09 +0530

    Add Dashboard to Vendor Portal

--- src/app/vendor-portal/dashboard/dashboard.component.ts
+++ src/app/vendor-portal/dashboard/dashboard.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-import { map, shareReplay } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-import { AuthService } from '../../shared/auth.service';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -16,26 +14,51 @@ export class DashboardComponent {
     private breakpointObserver: BreakpointObserver,
     private titleService: Title
   ) {
-    this.titleService.setTitle('Customer Portal');
+    this.titleService.setTitle('Vendor Portal');
   }
+
+  cardData = {
+    quotation: {
+      title: 'Request For Quotation',
+      url: '/vendor/quotation',
+      description:
+        'To request a company to  provide quotation and sales information for presales data collection.',
+      icon: 'receipt_long',
+      iconColor: '#8f56ff',
+    },
+    purchaseOrder: {
+      title: 'Purchase Order Data',
+      url: '/vendor/purchase-order',
+      description:
+        'To display the purchase orders recorded between the logon vendor and the company.',
+      icon: 'list_alt',
+      iconColor: '#007bff',
+    },
+    receipt: {
+      title: 'Goods Receipt',
+      url: '/vendor/receipt',
+      description:
+        'To display the Goods Receipt deliveries recorded from the company and the logon vendor.',
+      icon: 'featured_play_list',
+      iconColor: '#c656ff',
+    },
+  };
 
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          // { title: 'Card 3', cols: 1, rows: 1 },
-          // { title: 'Card 4', cols: 1, rows: 1 },
+          { ...this.cardData.quotation, cols: 1, rows: 1 },
+          { ...this.cardData.purchaseOrder, cols: 1, rows: 1 },
+          { ...this.cardData.receipt, cols: 1, rows: 1 },
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        // { title: 'Card 3', cols: 1, rows: 2 },
-        // { title: 'Card 4', cols: 1, rows: 1 },
+        { ...this.cardData.quotation, cols: 2, rows: 1 },
+        { ...this.cardData.purchaseOrder, cols: 1, rows: 1 },
+        { ...this.cardData.receipt, cols: 1, rows: 1 },
       ];
     })
   );
