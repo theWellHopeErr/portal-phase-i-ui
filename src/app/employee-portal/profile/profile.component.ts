171da1b509b6efe150c6cca45c1422450dbb29c8
theWellHopeErr <ssuryarajan@gmail.com>
2021-06-23 18:00:22 +0530

    Add Profile Component in Employee Portal

--- src/app/employee-portal/profile/profile.component.ts
+++ src/app/employee-portal/profile/profile.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+
+import { SnackService } from 'src/app/shared/snack.service';
+import { ProfileService } from '../services/profile.service';
+import { Profile } from './profile';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private profileService: ProfileService,
+    private snackService: SnackService,
+    private titleService: Title
+  ) {
+    this.titleService.setTitle('Profile | Employee Portal');
+    this.profileService.get().subscribe((res: Profile) => {
+      console.log(res);
+      this.form = res;
+      this.profileForm = this.formBuilder.group(this.form);
+      this.formLoading = false;
+      console.log(this.profileForm);
+    });
   }
 
+  profileForm: FormGroup;
+  isEditEnabled = false;
+  saveLoading = false;
+  formLoading = true;
+  panelOpenState = false;
+  form: Profile = {
+    eid: '',
+    name1: '',
+    name2: '',
+    title: '',
+    address: '',
+    city: '',
+    region: '',
+    p_code: '',
+    nationality: '',
+    tel: '',
+  };
+
+  ngOnInit(): void {
+    this.profileForm = this.formBuilder.group(this.form);
+  }
+
+  edit(): void {
+    console.log(this.isEditEnabled);
+    this.isEditEnabled = true;
+  }
+
+  save(): void {
+    this.saveLoading = true;
+
+    this.form = this.profileForm.value;
+    this.profileService.edit(this.profileForm.value).subscribe((res) => {
+      this.isEditEnabled = false;
+      this.saveLoading = false;
+      this.snackService.openSnackBar(`Profile Updated!!`);
+    });
+  }
+
+  cancel(): void {
+    this.profileForm.setValue(this.form);
+    this.isEditEnabled = false;
+  }
 }
---
+++ src/app/employee-portal/profile/profile.ts
@@ -0,0 +1,12 @@
+export interface Profile {
+  eid: string;
+  name1: string;
+  name2: string;
+  title: string;
+  address: string;
+  city: string;
+  region: string;
+  p_code: string;
+  nationality: string;
+  tel: string;
+}
