b55bcf9d2897d0c6600499ee6defaf2455fb6ae2
theWellHopeErr <ssuryarajan@gmail.com>
2021-07-04 17:58:30 +0530

    Minor UI Fix in Employee Portal

--- src/app/employee-portal/services/hcm.service.ts
+++ src/app/employee-portal/services/hcm.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LeaveReq } from '../leave-request/LeaveReq';
 
 @Injectable({
   providedIn: 'root',
@@ -10,21 +11,31 @@ export class HcmService {
   getLeaveData() {
     return this.http.get(`api/employee/leave-data`);
   }
+
+  createLeaveRequest(leave_req: LeaveReq) {
+    return this.http.post('api/employee/leave-req', leave_req);
+  }
+
   getPaySlipList() {
     return this.http.get(`api/employee/ps`);
   }
+
   getPaySlipDetails(seq_no) {
     return this.http.get(`api/employee/ps-details?sn=${seq_no}`);
   }
+
   checkNoticeStatus() {
     return this.http.put(`api/employee/notice`, { option: 'status' });
   }
+
   requestNotice() {
     return this.http.put(`api/employee/notice`, { option: 'request' });
   }
+
   cancelNotice() {
     return this.http.put(`api/employee/notice`, { option: 'cancel' });
   }
+
   getFinalSettlement() {
     return this.http.get(`api/employee/final-set`);
   }
