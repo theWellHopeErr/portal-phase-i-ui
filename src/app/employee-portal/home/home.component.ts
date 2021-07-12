c2452a2276c377b30d37aadd9873073ac309252a
theWellHopeErr <ssuryarajan@gmail.com>
2021-07-12 10:49:56 +0530

    Add Chat Bot in Employee Portal

--- src/app/employee-portal/home/home.component.ts
+++ src/app/employee-portal/home/home.component.ts
@@ -4,6 +4,7 @@ import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+
 import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
@@ -25,9 +26,16 @@ export class HomeComponent implements OnInit {
   panelOpenState = false;
   routerLink = this.router.url.split('/')[2];
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     const currentUser = this.authService.currentUserValue;
     if (currentUser) this.username = currentUser.username;
+
+    let s = document.createElement('script');
+    s.setAttribute('src', 'https://cdn.cai.tools.sap/webchat/webchat.js');
+    s.setAttribute('channelId', '0d8f91c0-9806-47ed-aada-51aab3ed0d77');
+    s.setAttribute('token', '7486c468c7776e3375f4602ead94e26f');
+    s.setAttribute('id', 'cai-webchat');
+    document.body.appendChild(s);
   }
 
   isHandset$: Observable<boolean> = this.breakpointObserver
